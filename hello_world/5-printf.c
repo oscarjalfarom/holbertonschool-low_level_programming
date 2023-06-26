e31ef757c8cc9c1cf29eef4f71ea863a755a6d9d
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-26 00:01:58 -0500

    sixth task done: 5. Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,19 +1,19 @@
 /*
-* File: 5-printf.c
-* Auth: Oscar J. Alfaro M
-*/
+ * File: 5-printf.c
+ * Auth: Oscar J Alfaro M
+ */
 
 #include <stdio.h>
 
 /**
-* main - Prints "with proper grammar, but the outcome is
-*                a piece of art," followed by a new line.
-*
-* Return: Always 0.
-*/
-
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
