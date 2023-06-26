e163ec323a851f9b88c7640fd7137eac9672e71c
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-25 23:44:06 -0500

    Remove completed task: 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-int main(void) {return puts("Programming is like building a multilingual puzzle"), 0; }
+/*
+ * File: 4-puts.c
+ * Auth: Oscar Julio Alfaro Montenegro
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
