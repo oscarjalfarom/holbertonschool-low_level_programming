ed4f9d60fee729c17625bfde62c5df9cccef42e5
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-26 22:19:56 -0500

    Task completed

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	printf("_putchar\n");
+
+return (0);
 }
