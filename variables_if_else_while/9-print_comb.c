bc65a34f2399526598c7b1a0342d1256e1051167
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-26 21:29:09 -0500

    Task completed

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,17 +12,20 @@
 
 int main(void)
 {
-	int n;
+	int c;
 
-	for (n = 0 ; n < 10 ; n++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar(c);
+
+		if (c != '9')
 		{
-		        putchar(',');
+			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
