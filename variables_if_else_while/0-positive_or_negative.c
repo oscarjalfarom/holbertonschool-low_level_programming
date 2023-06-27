c37b2464dd97f58da0046ffd78a14b53c2716bb3
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-26 20:37:19 -0500

    Task completed

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	printf("The number %d is ", n);
+	if (n > 0)
+		printf("positive\n");
+	else if (n < 0)
+		printf("negative\n");
+	else
+		printf("zero\n");
+
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+# Aprendiendo C: Variables, if, else, while
+
+Este repositorio es parte de mi proceso de aprendizaje de programación en el lenguaje C. En este proyecto, estoy explorando los conceptos fundamentales de C, centrándome en las variables y las estructuras de control como "if", "else" y "while".
