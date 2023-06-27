ba9885657a1090c7334bf537730929347ba9d8a7
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-26 22:48:20 -0500

    Task completed

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,25 +1,19 @@
 #include "main.h"
 /**
  * main - Entry point
+ * write _putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return 0;
-}
-
-int _putchar(char c)
-{
-    /* Your _putchar implementation goes here */
-    return write(1, &c, 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
