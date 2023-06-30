1c98121984e414a3ef2a7dcf859c49f3ea1f8c42
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-06-29 20:29:08 -0500

    Starting programming today in C - More functions, more nested loops - main.h.c, so cool

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,13 +1,1 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
- */
-
 int _putchar(char c);
-
-#endif
