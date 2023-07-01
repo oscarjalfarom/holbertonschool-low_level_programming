270676d15351610c6a1f4ab2571772dfb628a2ca
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-07-01 15:11:56 -0500

    Starting programming today in C - Pointers, arrays and strings, so cool

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  ** reset_to_98 - Updates the pointer value to 98
+  **@n: The pointer to update
+  **Return: 0
+  **/
+
+char *_strcat(char *dest, char *src)
+{
+    char *ptr = dest;
+
+    while (*ptr != '\0') {
+        ptr++;
+    }
+
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+
+    *ptr = '\0';
+
+    return dest;
+}
