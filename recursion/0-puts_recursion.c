ad4f3ed450498a1ac833b6c02597d49b4b78c8ef
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-07-04 22:47:27 -0500

    Starting programming today in C - Recursion -  0-puts_recursion.c, so cool

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * _puts_recursion - Entry Point
- *@s: input
- *Return: Always 0 success
- */
-void _puts_recursion(char *s)
+void
+_puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
-		return;
+		putchar('\n');
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	else
+	{
+		putchar(*s);
+		++s;
+		_puts_recursion(s);
+	}
 }
