2204e58a005a9e77d961cad791143632c44fff0c
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-07-05 21:59:14 -0500

    Starting programming today in C - Recursion -  1-print_rev_recursion.c, so cool

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,14 +1,19 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Oscar J Alfarom M
+ */
+
 #include "main.h"
 
 /**
- * _print_rev_recursion - Imprime una cadena en orden inverso.
- * @s: Puntero a la cadena a imprimir.
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-    	return;
-
-	_print_rev_recursion(s + 1);
-    _putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
